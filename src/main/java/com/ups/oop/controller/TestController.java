a40ea43c65a240067e1bd8d1bb3381bdf1986a0a
crisvalench <cristinavalenzuela2002@hotmail.com>
2024-06-17 08:31:28 -0500

    Add(II): Age change to optional

--- src/main/java/com/ups/oop/controller/TestController.java
+++ src/main/java/com/ups/oop/controller/TestController.java
@@ -16,9 +16,19 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(@RequestParam String name,
                         @RequestParam String lastname,
-                        @RequestParam Integer age){
-        return "This is my first SpringBootProject!" +
-                ", and my name is: "+ name + " " + lastname + " and I'm " + age + " years old.";
+                        @RequestParam(required = false) Integer age){
+        if (age == null){
+        return "This is my first SpringBootProject!" + ", and my name is: "+ name + " " + lastname; }
+        else{
+            return "This is my second rest service!, and my name is: "
+                    + name + " " + lastname +  " and I'm " + age + " years old.";
+        }
+    }
+
+    @GetMapping("/concat/{name}/{lastname}")
+    public String concatenate(@PathVariable String name, @PathVariable String lastname){
+        return "This is my second rest service!, and my name is: "
+                + name + " " + lastname;
     }
 
     @GetMapping("/concat/{name}/{lastname}/{age}")
